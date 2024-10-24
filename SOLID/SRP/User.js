119186304ffc7df8b9e4ae021d5b6f5ece97e43d
jhonchodev <jhonsis14@gmail.com>
2024-10-23 19:26:12 -0500

    Agregar comentarios SRP

--- SOLID/SRP/User.js
+++ SOLID/SRP/User.js
@@ -1,34 +1,43 @@
+// Ejemplo tomado de https://pieces.app/blog/solidifying-your-code-front-end-development-best-practices
 class User {
 
     constructor(name, email) {
-  
-      this.name = name;
-  
-      this.email = email;
-  
+
+        this.name = name;
+
+        this.email = email;
+
     }
-  
+
     saveUser() {
-  
-      // Logic to save user data to a database
-  
-      console.log("User saved successfully!");
-  
+
+        // Logic to save user data to a database
+
+        console.log("User saved successfully!");
+
     }
-  
+
     greet() {
-  
-      console.log(`Hello, my name is ${this.name}`);
-  
+
+        console.log(`Hello, my name is ${this.name}`);
+
     }
+
+}
+
+// Usage
+
+const user = new User("Alice", "alice@example.com");
+
+user.saveUser();
+
+user.greet();
+
+
+/*
   
-  }
-  
-  // Usage
-  
-  const user = new User("Alice", "alice@example.com");
-  
-  user.saveUser();
-  
-  user.greet();
-  
+* La clase de usuario se encarga de la gestión de los datos y las interacciones de los usuarios.
+
+* Esto viola el SRP porque la clase tiene múltiples motivos para cambiar. Cambia la forma en que almacenamos 
+* los datos de los usuarios y la lógica de interacción de los usuarios.  
+*/
--- SOLID/SRP/UserRefactor.js
+++ SOLID/SRP/UserRefactor.js
@@ -1,39 +1,50 @@
+// Ejemplo tomado de https://pieces.app/blog/solidifying-your-code-front-end-development-best-practices
+
 class User {
-  
+
     constructor(name, email) {
-  
-      this.name = name;
-  
-      this.email = email;
-  
+
+        this.name = name;
+
+        this.email = email;
+
     }
-  
+
     greet() {
-  
-      console.log(`Hello, my name is ${this.name}`);
-  
+
+        console.log(`Hello, my name is ${this.name}`);
+
     }
-  
-  }
-  
-  // User Service
-  
-  class UserService {
-  
+
+}
+
+// User Service
+
+class UserService {
+
     static saveUser(user) {
-  
-      // Logic to save user data to a database
-  
-      console.log("User saved successfully!");
-  
+
+        // Logic to save user data to a database
+
+        console.log("User saved successfully!");
+
     }
-  
-  }
-  
-  // Usage
-  
-  const user = new User("Alice", "alice@example.com");
-  
-  user.greet();
-  
-  UserService.saveUser(user);
+
+}
+
+// Usage
+
+const user = new User("Alice", "alice@example.com");
+
+user.greet();
+
+UserService.saveUser(user);
+
+
+/* 
+* La clase User proporciona un método de saludo y solo se encarga de representar los datos del usuario.
+
+* Introducimos la nueva clase UserService para gestionar las acciones relacionadas con el usuario, como guardar datos. 
+* De esta forma, cada clase tiene una única responsabilidad: la clase User gestiona la interacción del usuario, 
+* mientras que la clase UserService maneja la gestión de datos. 
+*/
